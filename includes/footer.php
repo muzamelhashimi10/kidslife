80031b31c4bae7ed29e7d0c5768d116de4197117
Hodor-Fergus <r0768810@student.thomasmore.be>
2021-05-18 01:30:20 +0200

    Footer Page using include or copy and paster directly into page

---
+++ includes/footer.php
@@ -0,0 +1,69 @@
+    <!-- Footer section start -->
+    <footer>
+        <div class="container">
+            <section style="height:80px;"></section>
+            <!----------- Footer ------------>
+            <footer class="footer-bs">
+                <div class="row">
+                    <div class="col-md-3 footer-brand animated fadeInLeft">
+                        <h2><img src="images/Logo 2.png" width="50px" height="50px" alt=""></h2>
+                        <p>Our vision is to be the source for childhood learning on the internet available from anywhere and without charge. Learning has to be fun! E-Learning for kids integrates gaming and learning to make our courseware more effective. To make the learning fun we present our courses as a-learning journey.</p>
+                      <p><strong>© 2021 KIDS LEARN, All rights reserved</strong></p>
+                    </div>
+                    <div class="col-md-3 footer-nav animated fadeInUp">
+                        <h4>Menu —</h4>
+                        <div class="col-md-6">
+                            <ul class="list">
+                                <li><a href="includes/about.php">About Us</a></li>
+                                <li><a href="includes/contact.php">Contacts</a></li>
+                                <li><a href="includes/terms.html">Terms & Condition</a></li>
+                                <li><a href="includes/privacy.html">Privacy Policy</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-md-3 footer-social animated fadeInDown">
+                        <h4>Follow Us</h4>
+                        <ul>
+                            <li><a href="facebook.com/kidslearn">Facebook</a></li>
+                            <li><a href="Twitter.com/kidslearn">Twitter</a></li>
+                            <li><a href="instagram.com/kidslearn">Instagram</a></li>
+                            <li><a href="mailto:info@muzamelhashimi10.be">Mail Us</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-3 footer-ns animated fadeInRight">
+                        <h4>Newsletter</h4>
+                        <p>Subscribe and connect with us for any update</p>
+                        <p>
+                            <div class="input-group">
+                                <form action="">
+                                    <input type="text" class="form-control" placeholder="Subscribe for...">
+                                    <span class="input-group-btn">
+                                      <button class="btn btn-default" type="button"></button>
+                                    </span>
+                                </form>
+                            </div><!-- /input-group -->
+                         </p>
+                    </div>
+                </div>
+            </footer>
+            <section style="text-align:center; margin:10px auto;"><p>Developer<a href="#"> D'FRAM BROTHERS 2021</a></p></section>
+        
+        </div>
+    </footer>
+    <!-- Footer section end -->
+    <!-- Bootstrap Bundle with Popper.js version 4/5 -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <!-- Popper JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+	<script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+    <!-- animation script -->
+    <script>			
+        AOS.init()({
+        offset:300,
+        duration:2000,
+        });
+</script>
+</body>
+</html>
