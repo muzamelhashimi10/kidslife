4e32ac7d9ea727c947a49496d1042c3075df3843
Muzamel Hashimi <muzamelhashimi10@gmail.com>
2021-05-18 15:05:04 +0200

    navaigation bar

---
+++ includes/navbar.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- Bootstrap version 4/5 -->
+    <link rel="stylesheet" type="text/css" href="bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <!-- Fonts awsome icons -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"/>
+    <!-- Jquery cdn Links for animation-->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+    <!-- css links -->
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body style="background-color: #e7e1e13f">
+    <!-- navbar start  -->
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top mb-2" style="background-color: rgba(75, 75, 180, 0.616);">
+        <div class="container">
+            <img src="images/Logo 1.png" width="40px" height="40px" alt=""> <span class="ml-1 text-white">kids Learn</span>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+          
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav ml-auto text-uppercase">
+                <li class="nav-item active">
+                  <a class="nav-link text-white" href="index.php">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link text-white" href="#course">Courses</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link text-white" href="includes/about.php">About Us</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="includes/team.php">Team</a>
+                </li>
+                  <li class="nav-item">
+                    <a class="nav-link text-white" href="includes/contact.php">Contact</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="includes/signup.php">SignUP/login</a>
+                </li>
+              </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- navbar end -->
